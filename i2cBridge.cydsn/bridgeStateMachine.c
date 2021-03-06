e819643d0aa14a1897e0332eeedf9670d339cca3
Chenthusiasm <vachen@gmail.com>
2021-03-05 22:54:45 -0600

    Start expanding the bridge state machine module's functionality.

--- i2cBridge.cydsn/bridgeStateMachine.c
+++ i2cBridge.cydsn/bridgeStateMachine.c
@@ -17,33 +17,63 @@
 
 // === TYPE DEFINES ============================================================
 
+/// The different states of the state machine.
 typedef enum State_
 {
+    /// Default/reset state.
+    State_Reset,
+    
+    /// Initialize the slave translator mode.
+    State_InitSlaveTranslator,
+    
+    /// Initialize the slave updater mode.
+    State_InitSlaveUpdater,
+    
+    /// Default I2C slave translator mode.
     State_SlaveTranslator,
+    
+    /// I2C slave update mode.
     State_SlaveUpdater,
 } State;
 
 
-// === DEFINES =================================================================
-
-#define DEFAULT_STATE                   (State_SlaveTranslator)
-
-
 // === GLOBAL VARIABLES ========================================================
 
-static State g_state = DEFAULT_STATE;
+/// The current state of the state machine.
+static State g_state = State_Reset;
 
 
 // === PRIVATE FUNCTIONS =======================================================
 
+/// Processes all tasks associated with initializing the I2C slave translator.
+void processInitSlaveTranslator(void)
+{
+}
 
+
+/// Processes all tasks associated with the I2C slave translator.
+void processSlaveTranslator(void)
+{
+}
+
+
+/// Processes all tasks associated with initializing the I2C slave updater.
+void processInitSlaveUpdater(void)
+{
+}
+
+
+/// Processes all tasks associated with the I2C slave updater.
+void processSlaveUpdater(void)
+{
+}
 
 
 // === PUBLIC FUNCTIONS ========================================================
 
 void bridgeStateMachine_reset(void)
 {
-    g_state = DEFAULT_STATE;
+    g_state = State_Reset;
 }
 
 
@@ -51,6 +81,24 @@ void bridgeStateMachine_process(void)
 {
     switch(g_state)
     {
+        case State_Reset:
+        {
+            g_state = State_InitSlaveTranslator;
+            break;
+        }
+        
+        case State_InitSlaveTranslator:
+        {
+            g_state = State_SlaveTranslator;
+            break;
+        }
+        
+        case State_InitSlaveUpdater:
+        {
+            g_state = State_SlaveUpdater;
+            break;
+        }
+        
         case State_SlaveTranslator:
         {
             break;
