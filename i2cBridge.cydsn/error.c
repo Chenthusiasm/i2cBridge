3192431b5bb7ae637261ce9d810e9f150612f16b
Chenthusiasm <vachen@gmail.com>
2021-04-09 23:23:26 -0500

    Added new functions to generate the error messages in the error module.

--- i2cBridge.cydsn/error.c
+++ i2cBridge.cydsn/error.c
@@ -31,9 +31,38 @@ ErrorMode error_getMode(void)
     return g_mode;
 }
 
+
 void error_setMode(ErrorMode mode)
 {
     g_mode = mode;
+}
+
+
+int error_makeSystemError(uint8_t buffer[], uint16_t size, uint8_t systemStatus, uint16_t callsite, bool cli)
+{
+    int status = -1;
+    return status;
+}
+
+
+int error_makeUpdaterError(uint8_t buffer[], uint16_t size, uint8_t updaterStatus, uint16_t callsite, bool cli)
+{
+    int status = -1;
+    return status;
+}
+
+
+int error_makeI2cError(uint8_t buffer[], uint16_t size, I2cGen2Status i2cStatus, uint32_t driverStatus, uint16_t callsite, bool cli)
+{
+    int status = -1;
+    return status;
+}
+
+
+int error_makeUartError(uint8_t buffer[], uint16_t size, uint8_t uartStatus, uint16_t callsite, bool cli)
+{
+    int status = -1;
+    return status;
 }
 
 
