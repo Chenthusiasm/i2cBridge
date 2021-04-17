205fd330e53d8610b35e671d05e2f88ff782834c
Chenthusiasm <vachen@gmail.com>
2021-04-17 05:11:01 -0500

    Added new module: I2C updater. No implementation yet.

---
+++ i2cBridge.cydsn/i2cUpdater.c
@@ -0,0 +1,30 @@
+/* ========================================
+ *
+ * UICO, 2021
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+// === DEPENDENCIES ============================================================
+
+#include "i2cUpdater.h"
+
+#include <limits.h>
+
+#include "hwSystemTime.h"
+
+
+// === DEFINES =================================================================
+
+
+
+// === PUBLIC FUNCTIONS ========================================================
+
+
+
+/* [] END OF FILE */
---
+++ i2cBridge.cydsn/i2cUpdater.h
@@ -0,0 +1,41 @@
+/* ========================================
+ *
+ * UICO, 2021
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+#ifndef I2C_UPDATER_H
+    #define I2C_UPDATER_H
+
+    #ifdef __cplusplus
+        extern "C" {
+    #endif
+    
+    // === DEPENDENCIES ========================================================
+    
+    #ifndef __cplusplus
+        #include <stdbool.h>
+    #endif
+    #include <stdint.h>
+    
+    
+    // === TYPE DEFINES ========================================================
+    
+    
+    // === FUNCTIONS ===========================================================
+    
+    
+    
+    #ifdef __cplusplus
+    } // extern "C"
+    #endif
+    
+#endif // I2C_UPDATER_H
+
+/* [] END OF FILE */
